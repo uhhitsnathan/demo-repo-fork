f6c1bc1ca92fe141f9f303ca32995e5f16da2b4b
uhhitsnathan <nathans2003@icloud.com>
2024-10-10 12:09:34 -0700

    yes

--- express/server.js
+++ express/server.js
@@ -3,13 +3,17 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get("/", (req, resp) => {
+    resp.sendFile(path.join(_dirname, 'public', 'index.html'))
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
