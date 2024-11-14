370cfb6cb3406256d751d53e01acead934988639
uhhitsnathan <nathans2003@icloud.com>
2024-11-14 11:32:34 -0800

    add /route

--- db-1/config/seed-db.js
+++ db-1/config/seed-db.js
@@ -4,6 +4,7 @@ const dropTables = async () => {
     try {
         console.log('dropping tables...');
         const dropTablesQuery = `
+            DROP TABLE IF EXISTS cats
         `;
         await pool.query(dropTablesQuery);
     } catch (error) {
@@ -15,8 +16,14 @@ const createTables = async () => {
     try {
         console.log('creating cats...');
         const createQuery = `
-
+            CREATE TABLE IF NOT EXISTS cats (
+                id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                name TEXT NOT NULL,
+                color TEXT NOT NULL,
+                human TEXT NOT NULL,
+                photo TEXT
             );
+            
         `;
         await pool.query(createQuery);
         console.log('created cats');
@@ -30,7 +37,9 @@ const insertData = async () => {
     try {
         console.log('adding initial data...');
         const insertQuery = `
-
+            INSERT INTO cats (name,color,human,photo) VALUES ('Jiji','black','Kiki','/images/jiji.png');
+            INSERT INTO cats (name,color,human,photo) VALUES ('Lion','pink','Steven','/images/lion.png');
+            INSERT INTO cats (name,color,human,photo) VALUES ('Calvin','orange','Hobbes','/images/calvin.png');
             
         `;
         await pool.query(insertQuery);
